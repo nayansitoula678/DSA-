d265d1a97b79ae8d69ac95f6ab8cc14cbb32a7c4
Nayan Sitoula <157961117+nayansitoula678@users.noreply.github.com>
2025-05-31 21:14:03 +0545

    linear and circular link list

---
+++ circular_linked_list.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+
+class CircularLinkedList {
+private:
+    Node* head;
+
+public:
+    CircularLinkedList() {
+        head = nullptr;
+    }
+
+    void append(int val) {
+        Node* newNode = new Node(val);
+        if (!head) {
+            head = newNode;
+            newNode->next = head;
+            head = newNode;
+            return;
+        }
+        Node* temp = head;
+        while (temp->next != head) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        newNode->next = head;
+    }
+
+    void display() {
+        if (!head) return;
+        Node* temp = head;
+        do {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        } while (temp != head);
+        cout << "(back to head)" << endl;
+    }
+};
+
+int main() {
+    CircularLinkedList list;
+    list.append(10);
+    list.append(20);
+    list.append(30);
+    list.display();
+    return 0;
+}
---
+++ linear_linked_list.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+
+class SinglyLinkedList {
+private:
+    Node* head;
+
+public:
+    SinglyLinkedList() {
+        head = nullptr;
+    }
+
+    void append(int val) {
+        Node* newNode = new Node(val);
+        if (!head) {
+            head = newNode;
+            return;
+        }
+        Node* temp = head;
+        while (temp->next) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+
+    void display() {
+        Node* temp = head;
+        while (temp) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+int main() {
+    SinglyLinkedList list;
+    list.append(10);
+    list.append(20);
+    list.append(30);
+    list.display();
+    return 0;
+}
